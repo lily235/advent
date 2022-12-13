02a745accfca35dc9296d437d0d591c575313453
lily235 <116597430+lily235@users.noreply.github.com>
2022-12-13 12:15:58 +0000

    the 2nd puzzle of Day 4

---
+++ Day4/day4_2.js
@@ -0,0 +1,34 @@
+function readInputAsLines(filename) {
+    try {
+      const fs = require('fs');
+      const data = fs.readFileSync(filename, 'utf8');
+      const lines = data.split("\n");
+      return lines;
+    } catch (err) {
+      console.error(err);
+    }
+    return [];
+  }
+
+
+function isFullyContained(pairSectionsArray){
+//when the 1st number is bigger than the 4rd number or when the 3st number is bigger than the 2rd number, return false
+if(pairSectionsArray[0] > pairSectionsArray[3] || pairSectionsArray[2] > pairSectionsArray[1] )
+{
+    return false;
+}else{
+    return true;
+}
+}
+
+var totalScore = 0;
+
+readInputAsLines('t.txt').forEach(line=> {
+
+if(isFullyContained(line.replace(',','-').split('-').map(Number))){
+    totalScore += 1;
+}   
+});
+  
+console.log('totalScore: ', totalScore);
+  
