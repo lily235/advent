94d07efa92dffd9e283bfce50ca0c22f4d1c0d72
lily235 <116597430+lily235@users.noreply.github.com>
2022-12-20 10:53:31 +0000

    prettier

--- Day7/day7.js
+++ Day7/day7.js
@@ -1,118 +1,104 @@
 function readInputAsLines(filename) {
-    try {
-      const fs = require('fs');
-      const data = fs.readFileSync(filename, 'utf8');
-      const lines = data.split("\n");
-      return lines;
-    } catch (err) {
-      console.error(err);
-    }
-    return [];
+  try {
+    const fs = require("fs");
+    const data = fs.readFileSync(filename, "utf8");
+    const lines = data.split("\n");
+    return lines;
+  } catch (err) {
+    console.error(err);
   }
+  return [];
+}
 
-var result=0;
+var result = 0;
 var dirctorySizes = {};
 var dirctorySize = 0;
-var directoryPath='';
-dirctorySizes['/'] = 0;
+var directoryPath = "";
+dirctorySizes["/"] = 0;
 
-function loopFileSystem(Path,filesize){
-    if(Path === '/'){
-       //dirctorySizes['/'] += filesize;
-        return;
+function loopFileSystem(Path, filesize) {
+  if (Path === "/") {
+    //dirctorySizes['/'] += filesize;
+    return;
+  }
+
+  let pathfiles = Path.split("/");
+
+  var CurPath = Path;
+  for (let i = pathfiles.length - 1; i > 0; i--) {
+    if (i === 1) {
+      dirctorySizes["/"] += filesize;
+    } else {
+      CurPath = CurPath.slice(0, -(pathfiles[i].length + 1));
+      dirctorySizes[CurPath] += filesize;
     }
+  }
+}
 
-    let pathfiles = Path.split('/');
-  
-    var CurPath = Path;
-    for(let i = pathfiles.length-1; i > 0; i--)
-    {
-        if(i===1){
-            dirctorySizes['/'] += filesize;
-        }else{
-            
-            CurPath = CurPath.slice(0,-(pathfiles[i].length + 1));
-            dirctorySizes[CurPath] += filesize;
-        }  
+function MoveOutOneLevel(Path) {
+  let pathfiles = Path.split("/");
+  return Path.slice(0, -(pathfiles[pathfiles.length - 1].length + 1));
+}
+
+readInputAsLines("input.txt").forEach((line) => {
+  if (line === "$ ls") {
+  } else if (line[0] === "$" && line.length > 4) {
+    if (line === "$ cd /") {
+      directoryPath = "/";
+    } else if (line === "$ cd ..") {
+      directoryPath = MoveOutOneLevel(directoryPath);
+    } else {
+      if (directoryPath === "/") {
+        directoryPath = `/${line.slice(5)}`;
+      } else {
+        //line = '$ cd e';
+        directoryPath = `${directoryPath}/${line.slice(5)}`;
+      }
     }
-}
-
-function MoveOutOneLevel(Path){
-    let pathfiles = Path.split('/');
-    return Path.slice(0,-(pathfiles[pathfiles.length-1].length + 1));
-}
-
-
-
-readInputAsLines('input.txt').forEach(line=>{
-    if(line === '$ ls'){
-
-    }else if(line[0] === '$' && line.length > 4){
-        if(line === '$ cd /'){
-            directoryPath = '/';
-            
-        }else if(line === '$ cd ..'){
-            directoryPath = MoveOutOneLevel(directoryPath);
-        }else{
-            if(directoryPath === '/'){
-                directoryPath = `/${line.slice(5)}`;
-            }else{
-                //line = '$ cd e';
-                directoryPath = `${directoryPath}/${line.slice(5)}`;
-            }
-        }
-        
-    }else if(line.slice(0,3) === 'dir'){
-        var filedirectory = '';
-        if(directoryPath === '/'){
-            filedirectory = `/${line.slice(4)}`;
-            dirctorySizes[filedirectory] = 0;
-        }else{
-            //line = 'dir e';
-            filedirectory = `${directoryPath}/${line.slice(4)}`;
-            dirctorySizes[filedirectory] = 0;
-        }
-    }else{
-        //file
-        let curFileSize = Number(line.split(' ')[0]);
-        dirctorySizes[directoryPath] += curFileSize;
-        loopFileSystem(directoryPath,curFileSize);
-}
-
-    
+  } else if (line.slice(0, 3) === "dir") {
+    var filedirectory = "";
+    if (directoryPath === "/") {
+      filedirectory = `/${line.slice(4)}`;
+      dirctorySizes[filedirectory] = 0;
+    } else {
+      //line = 'dir e';
+      filedirectory = `${directoryPath}/${line.slice(4)}`;
+      dirctorySizes[filedirectory] = 0;
+    }
+  } else {
+    //file
+    let curFileSize = Number(line.split(" ")[0]);
+    dirctorySizes[directoryPath] += curFileSize;
+    loopFileSystem(directoryPath, curFileSize);
+  }
 });
-
 
 //get a total size of at most 100000
 var mostSize = 100000;
-var result=0;
-var temptotalsize=0;
-var oneDiretorySizes=[];
+var result = 0;
+var temptotalsize = 0;
+var oneDiretorySizes = [];
 
-var tempDirectorySizes=[];
-
+var tempDirectorySizes = [];
 
 for (const pathKey in dirctorySizes) {
-    if(dirctorySizes[pathKey] < mostSize){
-        result +=dirctorySizes[pathKey];
-        oneDiretorySizes.push(dirctorySizes[pathKey]);
-    }
- }
- console.log(result);
+  if (dirctorySizes[pathKey] < mostSize) {
+    result += dirctorySizes[pathKey];
+    oneDiretorySizes.push(dirctorySizes[pathKey]);
+  }
+}
+console.log(result);
 
-var allDirectorySizes=[];
+var allDirectorySizes = [];
 //console.log(dirctorySizes);
-var needSize=dirctorySizes['/'] + 30000000 - 70000000;
+var needSize = dirctorySizes["/"] + 30000000 - 70000000;
 
- for (const pathKey in dirctorySizes) {
-    if(dirctorySizes[pathKey] >= needSize){
-        allDirectorySizes.push(dirctorySizes[pathKey]);
-    }
- }
+for (const pathKey in dirctorySizes) {
+  if (dirctorySizes[pathKey] >= needSize) {
+    allDirectorySizes.push(dirctorySizes[pathKey]);
+  }
+}
 
 var smallestNumber = Math.min(...allDirectorySizes);
 
 console.log(smallestNumber);
-
-
-
