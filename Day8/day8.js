ba2041a2b281787c0cc010b0c01f3456889c3aaa
lily235 <116597430+lily235@users.noreply.github.com>
2022-12-21 07:10:47 +0000

    refactor part 1 into functions

--- Day8/day8.js
+++ Day8/day8.js
@@ -1,3 +1,39 @@
+function isLeftVisible(rowNum, columnNum, array) {
+  for (let i = 0; i < columnNum; i++) {
+    if (array[rowNum][i] >= array[rowNum][columnNum]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isRightVisible(rowNum, columnNum, array) {
+  for (let i = columnNum + 1; i < array[0].length; i++) {
+    if (array[rowNum][i] >= array[rowNum][columnNum]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isTopVisible(rowNum, columnNum, array) {
+  for (let i = 0; i < rowNum; i++) {
+    if (array[i][columnNum] >= array[rowNum][columnNum]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isBottompVisible(rowNum, columnNum, array) {
+  for (let i = rowNum + 1; i < array.length; i++) {
+    if (array[i][columnNum] >= array[rowNum][columnNum]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function part1(input) {
   var visibleNum = 0;
   input.forEach((line, index, array) => {
@@ -6,36 +42,12 @@ function part1(input) {
     } else {
       visibleNum += 2;
       for (let i = 1; i < line.length - 1; i++) {
-        var leftvisible = true;
-        var rightvisible = true;
-        var topvisible = true;
-        var bottomvisible = true;
-        for (let j = 0; j < i; j++) {
-          if (array[index][j] >= array[index][i]) {
-            leftvisible = false;
-            break;
-          }
-        }
-        for (let j = i + 1; j < line.length; j++) {
-          if (array[index][j] >= array[index][i]) {
-            rightvisible = false;
-            break;
-          }
-        }
-        for (let j = 0; j < index; j++) {
-          if (array[j][i] >= array[index][i]) {
-            topvisible = false;
-            break;
-          }
-        }
-        for (let j = index + 1; j < array.length; j++) {
-          if (array[j][i] >= array[index][i]) {
-            bottomvisible = false;
-            break;
-          }
-        }
-
-        if (leftvisible || rightvisible || topvisible || bottomvisible) {
+        if (
+          isLeftVisible(index, i, array) ||
+          isRightVisible(index, i, array) ||
+          isTopVisible(index, i, array) ||
+          isBottompVisible(index, i, array)
+        ) {
           visibleNum += 1;
         }
       }
@@ -60,4 +72,4 @@ function readInputAsLines(filename) {
 }
 
 console.log(part1(readInputAsLines("input.txt")));
-console.log(part2(readInputAsLines("input.txt")));
+//console.log(part2(readInputAsLines("input.txt")));
